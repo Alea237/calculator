fdcefc6919007eeb76b63e09c4e377e77dee39f6
Adeola Bukoye <deoizbookoye@gmail.com>
2024-11-07 19:45:27 -0500

    substration function added

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let operator = ""
 function appendNumbers(number) {
     currentInput += number
     document.getElementById("display").value = currentInput
-    console.log("number = "+currentInput);
+    console.log("number = " + currentInput);
 }
 
 function addition() {
@@ -13,17 +13,26 @@ function addition() {
     document.getElementById("display").value = currentInput
 }
 
+function substraction() {
+    currentInput += "-"
+    operator = "-"
+    document.getElementById("display").value = currentInput
+}
+
 function calculate() {
 
-    let numbers = currentInput.split("+")
+    let numbers = currentInput.split(operator)
     let num1 = parseFloat(numbers[0])
     let num2 = parseFloat(numbers[1])
     let result = 0
 
-    switch(operator) {
-        case "+": 
-        result = num1 + num2
-        break;
+    switch (operator) {
+        case "+":
+            result = num1 + num2
+            break;
+        case "-":
+            result = num1 - num2
+            break;
     }
 
     document.getElementById("display").value = result
